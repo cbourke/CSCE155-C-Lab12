329a433f7dc68e1fbb3321883b18707dd0c16695
Christopher Bourke <cbourke@cse.unl.edu>
2015-05-19 21:41:04 -0500

    Initial Commit

---
+++ palindrome.c
@@ -0,0 +1,30 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int isPalindrome(const char *str, int left_index, int right_index);
+
+int main(int argc, char **argv) {
+
+  if(argc != 2) {
+    printf("Usage: isPalindrome word\n");
+    exit(1);
+  }
+  
+  int isPal = isPalindrome(argv[1], 0, strlen(argv[1])-1);
+  if(isPal) {
+    printf("%s is a palindrome!\n", argv[1]);
+  } else {
+    printf("%s is NOT a palindrome!\n", argv[1]);
+  }
+  return 0;
+}
+
+/**
+ * This function should return true if the given (sub)string is 
+ * a palindrome.  The function should be recursive.
+ */
+int isPalindrome(const char *str, int left_index, int right_index) {
+  //TODO: implement this function as specified
+}
---
+++ recursiveFunction.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int recursiveFunction(int n);
+
+int main(int argc, char **argv) {
+
+  if(argc != 2) {
+    fprintf(stderr, "Usage: %s n\n", argv[0]);
+    exit(1);
+  }
+  int n = atoi(argv[1]);
+
+  time_t start, end;
+  start = time(NULL); 
+  int result = recursiveFunction(n);
+  end = time(NULL);
+
+  int time = (end - start);
+
+  printf("f(%d) = %d\n", n, result);
+
+  printf("Computation Time: %d seconds\n", time);
+
+  return 0;
+}
+
+int recursiveFunction(int n) {
+  if(n == 0) {
+    return 2;
+  } else if(n == 1) {
+    return 2;
+  } else {
+    return recursiveFunction(n-1) + (recursiveFunction(n - 2) / 2);
+  }
+
+}
+
